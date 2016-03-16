46a2cd6a4a8bc9fcc3ef439e4400c1a3d91542e8
Mikron <mikronia@gmail.com>
2016-03-16 23:20:05 +0100

    Translation config added

---
+++ common/config/i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Configuration file for 'yii message/extract' command
+ */
+return [
+    'sourcePath' => __DIR__. '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
+    'languages' => ['en', 'pl'],
+    'translator' => 'Yii::t',
+    'sort' => true,
+    'removeUnused' => true,
+    'only' => ['*.php'],
+    'except' => [
+        '.git',
+        '.gitignore',
+        '.gitkeep',
+        '/messages',
+        '/vendor',
+    ],
+    'format' => 'php',
+    'messagePath' => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'messages',
+    'overwrite' => true,
+];
