a58acf528dc0d4214cba11891114e85d9d9c7ebd
Mikron <mikronia@gmail.com>
2016-05-07 22:47:02 +0200

    Some styling for backend story view

--- backend/views/story/view.php
+++ backend/views/story/view.php
@@ -16,54 +16,68 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p class="text-right">
-        <?= Html::a(Yii::t('app', 'BUTTON_UPDATE'), ['update', 'id' => $model->story_id],
-            ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(
+            Yii::t('app', 'BUTTON_UPDATE'),
+            ['update', 'id' => $model->story_id],
+            ['class' => 'btn btn-primary']);
+        ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'key',
-            'data:ntext',
-        ],
-    ]) ?>
+    <div class="col-lg-6">
 
-    <h2><?php echo $model->getAttributeLabel('short'); ?></h2>
+        <h2><?= Yii::t('app', 'STORY_BASICS_HEADER'); ?></h2>
 
-    <div>
-        <?php echo $model->short; ?>
-    </div>
+        <p><b><?= $model->getAttributeLabel('key'); ?>:</b> <?= $model->key; ?></p>
 
-    <h2><?php echo $model->getAttributeLabel('long'); ?></h2>
+        <p class="text-left">
+            <b><?= $model->getAttributeLabel('data'); ?> (JSON):</b> <?= $model->data; ?>
+        </p>
 
-    <div>
-        <?php echo $model->long; ?>
-    </div>
+        <h2><?php echo $model->getAttributeLabel('storyParameters'); ?></h2>
 
-    <h2><?php echo $model->getAttributeLabel('storyParameters'); ?></h2>
+        <p class="text-right">
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_STORY_PARAMETER_CREATE'),
+                ['create-parameter'],
+                ['class' => 'btn btn-success']);
+            ?>
+        </p>
 
-    <p class="text-right">
-        <?= Html::a(Yii::t('app', 'BUTTON_STORY_PARAMETER_CREATE'), ['create-parameter'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => new \yii\data\ActiveDataProvider(['query' => \common\models\StoryParameter::find()->with('story')->where(['story_id' => $model->story_id])]),
-        'summary' => '',
-        'columns' => [
-            'code',
-            'content',
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{update-parameter}',
-                'buttons' => [
-                    'update-parameter' => function ($url, $model, $key) {
-                        return Html::a('<span class="glyphicon glyphicon-cog"></span>', $url, [
-                            'title' => Yii::t('app', 'View'),
-                        ]);
-                    }
-                ]
+        <?= GridView::widget([
+            'dataProvider' => new \yii\data\ActiveDataProvider(['query' => \common\models\StoryParameter::find()->with('story')->where(['story_id' => $model->story_id])]),
+            'summary' => '',
+            'columns' => [
+                'code',
+                'content',
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{update-parameter}',
+                    'buttons' => [
+                        'update-parameter' => function ($url, $model, $key) {
+                            return Html::a('<span class="glyphicon glyphicon-cog"></span>', $url, [
+                                'title' => Yii::t('app', 'View'),
+                            ]);
+                        }
+                    ]
+                ],
             ],
-        ],
-    ]); ?>
+        ]); ?>
+
+        <h2><?php echo $model->getAttributeLabel('short'); ?></h2>
+
+        <div>
+            <?php echo $model->short; ?>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+
+        <h2><?php echo $model->getAttributeLabel('long'); ?></h2>
+
+        <div>
+            <?php echo $model->long; ?>
+        </div>
+
+    </div>
 
 </div>
