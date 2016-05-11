7c0098791d06c5ca25207e2ff36b610fded97c8a
Mikron <mikronia@gmail.com>
2016-05-12 00:09:54 +0200

    Story parameters formalised

--- common/models/StoryParameter.php
+++ common/models/StoryParameter.php
@@ -17,12 +17,43 @@ use Yii;
  */
 class StoryParameter extends \yii\db\ActiveRecord
 {
+    const PARAMETER_STORY_NUMBER = 'story-number';
+    const PARAMETER_TIME_RANGE = 'time-range';
+    const PARAMETER_POINT_START = 'point-start';
+    const PARAMETER_POINT_END = 'point-end';
+    const PARAMETER_SESSION_COUNT = 'session-count';
+    const PARAMETER_PCS_ACTIVE = 'active-pcs';
+    const PARAMETER_CS_ACTIVE = 'active-cs';
+
+    private $allowedCodes = [
+        self::PARAMETER_STORY_NUMBER,
+        self::PARAMETER_TIME_RANGE,
+        self::PARAMETER_POINT_START,
+        self::PARAMETER_POINT_END,
+        self::PARAMETER_SESSION_COUNT,
+        self::PARAMETER_PCS_ACTIVE,
+        self::PARAMETER_CS_ACTIVE,
+    ];
+
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
         return 'story_parameter';
+    }
+
+    static public function codeNames()
+    {
+        return [
+            self::PARAMETER_STORY_NUMBER => Yii::t('app', 'ST_PARAM_STORY_NUMBER'),
+            self::PARAMETER_TIME_RANGE => Yii::t('app', 'ST_PARAM_TIME_RANGE'),
+            self::PARAMETER_POINT_START => Yii::t('app', 'ST_PARAM_POINT_START'),
+            self::PARAMETER_POINT_END => Yii::t('app', 'ST_PARAM_POINT_END'),
+            self::PARAMETER_SESSION_COUNT => Yii::t('app', 'ST_PARAM_SESSION_COUNT'),
+            self::PARAMETER_PCS_ACTIVE => Yii::t('app', 'ST_PARAM_PCS_ACTIVE'),
+            self::PARAMETER_CS_ACTIVE => Yii::t('app', 'ST_PARAM_CS_ACTIVE'),
+        ];
     }
 
     /**
@@ -36,7 +67,20 @@ class StoryParameter extends \yii\db\ActiveRecord
             [['visibility'], 'string'],
             [['code'], 'string', 'max' => 20],
             [['content'], 'string', 'max' => 80],
-            [['story_id'], 'exist', 'skipOnError' => true, 'targetClass' => Story::className(), 'targetAttribute' => ['story_id' => 'story_id']],
+            [
+                ['story_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => Story::className(),
+                'targetAttribute' => ['story_id' => 'story_id']
+            ],
+            [
+                ['code'],
+                'in',
+                'range' => function ($model, $attribute) {
+                    return $this->allowedCodes;
+                }
+            ],
         ];
     }
 
