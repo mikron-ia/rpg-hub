8156d89ced88c74a488ae8bde4634ce0aca5de9a
Mikron <mikronia@gmail.com>
2016-05-18 10:07:08 +0200

    Epic table expanded
    Concerns #24

--- console/migrations/m160517_212928_v0_1_dev_1.php
+++ console/migrations/m160517_212928_v0_1_dev_1.php
@@ -13,9 +13,11 @@ class m160517_212928_v0_1_dev_1 extends Migration
             $this->createTable('{{%description_pack}}', [
                 'epic_id' => $this->primaryKey()->unsigned(),
                 'key' => $this->string(80)->notNull(),
-                'name' => $this->string(80)->notNull(),
-                'short' => $this->text()->notNull(),
-                'long' => $this->text(),
+                'name' => $this->string(80)->notNull()->comment('Public name for the epic'),
+                'short' => $this->text()->notNull()->comment('Public concept'),
+                'notes' => $this->text()->notNull()->comment('GM notes regarding content'),
+                'declaration' => $this->text()->comment('Markdown-coded writing delineating basic concept of the campaign; this is public'),
+                'system' => $this->string(20)->comment('Code for the system used'),
             ], $tableOptions);
         }
     }
