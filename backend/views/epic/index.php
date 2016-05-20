d3f45b35b262487f5a4691714406aa3688e44db2
Mikron <mikronia@gmail.com>
2016-05-20 18:00:31 +0200

    Few adjustments to Epic control
    Concerns #24

--- backend/views/epic/index.php
+++ backend/views/epic/index.php
@@ -13,23 +13,21 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="epic-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Epic'), ['create'], ['class' => 'btn btn-success']) ?>
+    <p class="pull-right">
+        <?= Html::a(Yii::t('app', 'BUTTON_EPIC_ADD'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        'summary' => '',
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'epic_id',
             'key',
             'name',
             'system',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update}'
+            ],
         ],
     ]); ?>
 </div>
