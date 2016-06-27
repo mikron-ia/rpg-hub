4e74e6535e8485ce80d6dc9e435a486a19c91f34
Mikron <mikronia@gmail.com>
2016-06-27 23:44:45 +0200

    Parameters can be added to Epic
    Types still indifferential

--- backend/views/epic/view.php
+++ backend/views/epic/view.php
@@ -1,6 +1,7 @@
 <?php
 
 use common\models\Group;
+use common\models\Parameter;
 use common\models\Story;
 use yii\grid\GridView;
 use yii\helpers\Html;
@@ -36,7 +37,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="buttoned-header">
             <h2><?= Yii::t('app', 'EPIC_HEADER_GROUPS'); ?></h2>
-            <?= Html::a(Yii::t('app', 'BUTTON_GROUP_CREATE'), ['group/create'], ['class' => 'btn btn-success pull-right']); ?>
+            <?= Html::a(Yii::t('app', 'BUTTON_GROUP_CREATE'), ['group/create'],
+                ['class' => 'btn btn-success pull-right']); ?>
         </div>
 
         <?= GridView::widget([
@@ -74,7 +76,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="buttoned-header">
             <h2><?= Yii::t('app', 'EPIC_HEADER_STORIES'); ?></h2>
-            <?= Html::a(Yii::t('app', 'BUTTON_STORY_CREATE'), ['story/create'], ['class' => 'btn btn-success pull-right']); ?>
+            <?= Html::a(Yii::t('app', 'BUTTON_STORY_CREATE'), ['story/create'],
+                ['class' => 'btn btn-success pull-right']); ?>
         </div>
         <?= GridView::widget([
             'dataProvider' => new \yii\data\ActiveDataProvider([
@@ -151,5 +154,121 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]); ?>
     </div>
+
+    <div class="col-lg-12">
+
+        <div class="buttoned-header">
+            <h2><?= Yii::t('app', 'PARAMETER_TITLE_INDEX') ?></h2>
+            <?= Html::a(
+                '<span class="btn btn-success">' . Yii::t('app', 'BUTTON_PARAMETER_CREATE') . '</span>',
+                '#',
+                [
+                    'class' => 'create-parameter-link',
+                    'title' => Yii::t('app', 'BUTTON_PARAMETER_CREATE'),
+                    'data-toggle' => 'modal',
+                    'data-target' => '#create-parameter-modal'
+                ]
+            ); ?>
+        </div>
+
+        <?= GridView::widget([
+            'dataProvider' => new \yii\data\ActiveDataProvider(['query' => Parameter::find()->where(['parameter_pack_id' => $model->parameter_pack_id])]),
+            'summary' => '',
+            'columns' => [
+                [
+                    'attribute' => 'code',
+                    'enableSorting' => false,
+                    'value' => function (Parameter $model) {
+                        return $model->getCodeName();
+                    },
+                ],
+                [
+                    'attribute' => 'visibility',
+                    'enableSorting' => false,
+                    'value' => function (Parameter $model) {
+                        return $model->getVisibilityName();
+                    },
+                ],
+                [
+                    'attribute' => 'content',
+                    'enableSorting' => false,
+                ],
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{update} {delete}',
+                    'buttons' => [
+                        'update' => function ($url, Parameter $model, $key) {
+                            return Html::a('<span class="glyphicon glyphicon-cog"></span>', '#', [
+                                'class' => 'update-parameter-link',
+                                'title' => Yii::t('app', 'LABEL_UPDATE'),
+                                'data-toggle' => 'modal',
+                                'data-target' => '#update-parameter-modal',
+                                'data-id' => $key,
+                            ]);
+                        },
+                        'delete' => function ($url, Parameter $model, $key) {
+                            return Html::a(
+                                '<span class="glyphicon glyphicon-erase"></span>',
+                                ['parameter/delete', 'id' => $model->parameter_id],
+                                [
+                                    'title' => Yii::t('app', 'LABEL_DELETE'),
+                                    'data-confirm' => Yii::t(
+                                        'app',
+                                        'CONFIRMATION_DELETE {name}',
+                                        ['name' => $model->getCodeName()]
+                                    ),
+                                    'data-method' => 'post',
+                                ]);
+                        }
+                    ]
+                ],
+            ],
+        ]); ?>
+    </div>
+
+    <?php \yii\bootstrap\Modal::begin([
+        'id' => 'create-parameter-modal',
+        'header' => '<h2 class="modal-title">' . Yii::t('app', 'PARAMETER_TITLE_CREATE') . '</h2>',
+    ]); ?>
+
+    <?php \yii\bootstrap\Modal::end(); ?>
+
+    <?php $this->registerJs(
+        "$('.create-parameter-link').click(function() {
+    $.get(
+        '" . Yii::$app->urlManager->createUrl(['parameter/create']) . "',
+        {
+            pack_id: " . $model->parameterPack->parameter_pack_id . "
+        },
+        function (data) {
+            $('.modal-body').html(data);
+            $('#create-parameter-modal').modal();
+        }
+    );
+});"
+    ); ?>
+
+    <?php \yii\bootstrap\Modal::begin([
+        'id' => 'update-parameter-modal',
+        'header' => '<h2 class="modal-title">' . Yii::t('app', 'PARAMETER_TITLE_UPDATE') . '</h2>',
+    ]); ?>
+
+    <?php \yii\bootstrap\Modal::end(); ?>
+
+    <?php $this->registerJs(
+        "$('.update-parameter-link').click(function() {
+    $.get(
+        '" . Yii::$app->urlManager->createUrl(['parameter/update']) . "',
+        {
+            id: $(this).closest('tr').data('key')
+        },
+        function (data) {
+            $('.modal-body').html(data);
+            $('#update-parameter-modal').modal();
+        }
+    );
+});"
+    );
+    ?>
 
 </div>
