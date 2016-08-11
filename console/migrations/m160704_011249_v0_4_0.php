df529f0eb52287661e5793c83ff11254e8d2680b
Mikron <mikronia@gmail.com>
2016-08-12 00:29:54 +0200

    Table for activity log
    Concerns #39

--- console/migrations/m160704_011249_v0_4_0.php
+++ console/migrations/m160704_011249_v0_4_0.php
@@ -36,6 +36,15 @@ class m160704_011249_v0_4_0 extends m140506_102106_rbac_init
         $this->addColumn('{{%epic}}', 'main_gm_id', $this->integer(11)->unsigned());
         $this->addForeignKey('epic_fk_gm', '{{%epic}}', 'main_gm_id', '{{%gm}}', 'gm_id', 'RESTRICT', 'CASCADE');
 
+        $this->createTable('{{%user_action}}', [
+            'user_action_id' => $this->primaryKey()->unsigned(),
+            'user_id' => $this->integer(11)->unsigned()->notNull(),
+            'operation' => $this->string(80)->notNull(),
+            'class' => $this->string(80)->notNull(),
+            'id' => $this->integer(11)->unsigned()->notNull(),
+            'FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE RESTRICT ON UPDATE CASCADE',
+        ], $tableOptions);
+
         parent::up();
     }
 
@@ -61,12 +70,14 @@ class m160704_011249_v0_4_0 extends m140506_102106_rbac_init
         $this->dropTable('{{%gm}}');
         $this->dropTable('{{%player}}');
 
-        $this->execute('SET foreign_key_checks = 1;');
+        $this->dropTable('{{%user_action}}');
 
         $this->addColumn('description_pack', 'name', $this->string(80)->notNull());
         $this->addColumn('parameter_pack', 'name', $this->string(80)->notNull());
 
         $this->dropForeignKey('epic_fk_gm', '{{%epic}}');
         $this->dropColumn('{{%epic}}', 'main_gm_id');
+
+        $this->execute('SET foreign_key_checks = 1;');
     }
 }
