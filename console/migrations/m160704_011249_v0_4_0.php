4dfd1773601bfa1e6f87ef80685ac5b36e74f7a8
Mikron <mikronia@gmail.com>
2016-08-18 21:42:11 +0200

    Reworked role attribution - migration part done

--- console/migrations/m160704_011249_v0_4_0.php
+++ console/migrations/m160704_011249_v0_4_0.php
@@ -15,13 +15,16 @@ class m160704_011249_v0_4_0 extends m140506_102106_rbac_init
             'participant_id' => $this->primaryKey()->unsigned(),
             'user_id' => $this->integer(11)->unsigned()->notNull(),
             'epic_id' => $this->integer(11)->unsigned()->notNull(),
-            'role' => $this->string(20)->notNull(),
             'FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE RESTRICT ON UPDATE CASCADE',
             'FOREIGN KEY (epic_id) REFERENCES `epic` (epic_id) ON DELETE RESTRICT ON UPDATE CASCADE',
         ], $tableOptions);
 
-        $this->addColumn('{{%epic}}', 'main_gm_id', $this->integer(11)->unsigned());
-        $this->addForeignKey('epic_fk_gm', '{{%epic}}', 'main_gm_id', '{{%user}}', 'id', 'RESTRICT', 'CASCADE');
+        $this->createTable('{{%participant_role}}', [
+            'participant_id' => $this->integer(11)->unsigned()->notNull(),
+            'role' => $this->string(20)->notNull(),
+            'PRIMARY KEY (participant_id, role)',
+            'FOREIGN KEY (participant_id) REFERENCES `participant` (participant_id) ON DELETE RESTRICT ON UPDATE CASCADE',
+        ], $tableOptions);
 
         /**
          * Activity log table
@@ -68,14 +71,12 @@ class m160704_011249_v0_4_0 extends m140506_102106_rbac_init
         $this->truncateTable('{{%user}}');
 
         $this->dropTable('{{%participant}}');
+        $this->dropTable('{{%participant_role}}');
 
         $this->dropTable('{{%user_action}}');
 
         $this->addColumn('description_pack', 'name', $this->string(80)->notNull());
         $this->addColumn('parameter_pack', 'name', $this->string(80)->notNull());
-
-        $this->dropForeignKey('epic_fk_gm', '{{%epic}}');
-        $this->dropColumn('{{%epic}}', 'main_gm_id');
 
         $this->execute('SET foreign_key_checks = 1;');
     }
