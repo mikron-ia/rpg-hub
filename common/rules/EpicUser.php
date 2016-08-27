f11d75d857186a612a3b880ce69d96c3ff0f9aeb
Mikron <mikronia@gmail.com>
2016-08-27 21:20:59 +0200

    Rules for Epic user added

--- common/rules/EpicUser.php
+++ common/rules/EpicUser.php
@@ -2,15 +2,34 @@
 
 namespace common\rules;
 
+use common\models\Participant;
+use common\models\ParticipantRole;
 use yii\rbac\Rule;
 
-class EpicUser extends Rule
+abstract class EpicUser extends Rule
 {
     /**
      * @inheritdoc
      */
     public function execute($user, $item, $params)
     {
-        return false;
+        /* @var $participant Participant */
+        $participant = Participant::findOne([
+            'epic_id' => $params['epic']->epic_id,
+            'user_id' => $user
+        ]);
+
+        if (!$participant) {
+            return false;
+        }
+
+        $role = ParticipantRole::findOne([
+            'participant_id' => $participant->participant_id,
+            'role' => $this->requiredRole()
+        ]);
+
+        return ($role !== null);
     }
+
+    abstract public function requiredRole();
 }
