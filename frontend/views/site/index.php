e6e12d3a202102a9dc3be93e31a6693a7d08480c
Mikron <mikronia@gmail.com>
2016-10-28 00:58:39 +0200

    Front page layout reworked for mobiles
    Concerns #69

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,28 +11,23 @@ $this->title = 'RPG hub - index';
     <div class="jumbotron">
         <h1><?= Yii::t('app', 'FRONTPAGE_TITLE') ?></h1>
         <p class="lead"><?= Yii::t('app', 'FRONTPAGE_LEAD_TEXT') ?></p>
-    </div>
+        <div class="btn-group btn-group-lg">
+            <?= Html::a(Yii::t('app', 'BUTTON_STORIES'), ['story/index'], ['class' => 'btn btn-lg btn-success']); ?>
+        </div>
 
-    <div class="col-md-3">
-        <h2 class="text-center"><?= Yii::t('app', 'FRONTPAGE_MENU') ?></h2>
-        <div>
-            <p>
-                <?= Html::a(
-                    Yii::t('app', 'BUTTON_FRONTPAGE_STORIES'),
-                    ['story/index'],
-                    ['class' => 'btn btn-lg btn-success']
-                ); ?>
-            </p>
-            <p>
-                <?= Html::a(
-                    Yii::t('app', 'BUTTON_FRONTPAGE_PEOPLE'),
-                    ['people/index'],
-                    ['class' => 'btn btn-lg btn-success']
-                ); ?>
-            </p>
+        <div class="btn-group btn-group-lg">
+            <?= Html::a(Yii::t('app', 'BUTTON_PEOPLE'), ['person/index'], ['class' => 'btn btn-lg btn-success']); ?>
         </div>
     </div>
-    <div class="col-md-3">
 
+    <div class="col-md-6">
+        <h2 class="text-center"><?= Yii::t('app', 'FRONTPAGE_IC') ?></h2>
+        <h3><?= Yii::t('app', 'FRONTPAGE_WHAT_HAPPENED') ?></h3>
+    </div>
+
+    <div class="col-md-6">
+        <h2 class="text-center"><?= Yii::t('app', 'FRONTPAGE_OOC') ?></h2>
+        <h3><?= Yii::t('app', 'FRONTPAGE_SESSIONS') ?></h3>
+        <h3><?= Yii::t('app', 'FRONTPAGE_NEWS') ?></h3>
     </div>
 </div>
