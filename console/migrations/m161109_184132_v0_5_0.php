00ccc38662d4035d1d28fa8fba752caeb8f70bcb
Mikron <mikronia@gmail.com>
2016-11-10 20:01:27 +0100

    Migration for external data

--- console/migrations/m161109_184132_v0_5_0.php
+++ console/migrations/m161109_184132_v0_5_0.php
@@ -6,6 +6,25 @@ class m161109_184132_v0_5_0 extends Migration
 {
     public function up()
     {
+        $tableOptions = "CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB";
+
+        /* external_datas */
+        $this->createTable('{{%external_data_pack}}', [
+            'external_data_pack_id' => $this->primaryKey()->unsigned(),
+            'class' => $this->string(20)->notNull()->comment("Name of class this pack belongs to; necessary for proper type assignment"),
+            'name' => $this->string(80)->notNull(),
+        ], $tableOptions);
+
+        $this->createTable('{{%external_data}}', [
+            'external_data_id' => $this->primaryKey()->unsigned(),
+            'external_data_pack_id' => $this->integer(11)->unsigned(),
+            'code' => $this->string(40)->notNull(),
+            'data' => $this->text()->notNull(),
+            'visibility' => $this->string(20)->notNull()->defaultValue(\common\models\core\Visibility::VISIBILITY_GM),
+        ], $tableOptions);
+
+        $this->addColumn('person', 'external_data_pack_id', $this->integer(11)->unsigned());
+
         /**
          * Loading up data, if available
          * This is a stopgap measure that should be moved forward to newest migration and removed no later than in 1.0
@@ -37,6 +56,11 @@ class m161109_184132_v0_5_0 extends Migration
         $this->truncateTable('{{%story}}');
         $this->truncateTable('{{%story_parameter}}');
         $this->truncateTable('{{%user}}');
+
+        $this->dropColumn('person', 'external_data_pack_id');
+
+        $this->dropTable('{{%external_data}}');
+        $this->dropTable('{{%external_data_pack}}');
 
         $this->execute('SET foreign_key_checks = 1;');
     }
