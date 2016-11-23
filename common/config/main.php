467cb74603d58dbc61411063a6a319d65b563d83
Mikron <mikronia@gmail.com>
2016-11-23 16:19:00 +0100

    E-mail is now configurable
    Closes #89

--- common/config/main.php
+++ common/config/main.php
@@ -1,4 +1,24 @@
 <?php
+
+$mailer = [
+    'class' => 'yii\swiftmailer\Mailer',
+    'viewPath' => '@common/mail',
+];
+
+if (getenv('MAIL_SMTP')) {
+        $mailer['useFileTransport'] = false;
+        $mailer['transport'] = [
+            'class' => 'Swift_SmtpTransport',
+            'host' => getenv('MAIL_HOST'),
+            'username' => getenv('MAIL_USER'),
+            'password' => getenv('MAIL_PASS'),
+            'port' => getenv('MAIL_PORT'),
+            'encryption' => getenv('MAIL_ENCRYPT'),
+        ];
+} else {
+    $mailer['useFileTransport'] = true;
+}
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'language' => 'en',
@@ -36,6 +56,7 @@ return [
                 ],
             ],
         ],
+        'mailer' => $mailer,
     ],
     'modules' => [],
 ];
