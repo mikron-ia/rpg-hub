bfad60b1909f57dcb09b7bb69bfcb7b18cf3ad49
Mikron <mikronia@gmail.com>
2016-12-13 00:41:11 +0100

    Sightings are now visible on Character page
    Concerns #78

--- backend/views/character/_view_basic.php
+++ backend/views/character/_view_basic.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model common\models\Description */
+/* @var $model common\models\Character */
 
 ?>
 
--- backend/views/character/_view_views.php
+++ backend/views/character/_view_views.php
@@ -1,8 +1,27 @@
 <?php
+use common\models\Seen;
+
 /* @var $this yii\web\View */
-/* @var $model common\models\Description */
+/* @var $model common\models\Character */
 ?>
 
 <div>
-
+    <?= \yii\grid\GridView::widget([
+        'dataProvider' => new \yii\data\ActiveDataProvider([
+            'query' => $model->seenPack->getSightings(),
+            'pagination' => false,
+        ]),
+        'layout' => '{items}',
+        'columns' => [
+            'user.username',
+            'noted_at:datetime',
+            'seen_at:datetime',
+            [
+                'attribute' => 'status',
+                'value' => function(Seen $model) {
+                    return $model->getName();
+                }
+            ],
+        ],
+    ]) ?>
 </div>
