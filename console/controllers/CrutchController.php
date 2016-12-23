27ab4f0972279de96e23a5272e80710b494fc09c
Mikron <mikronia@gmail.com>
2016-12-24 00:13:56 +0100

    Crutch model restored for development

--- console/controllers/CrutchController.php
+++ console/controllers/CrutchController.php
@@ -17,4 +17,90 @@ use yii\console\Controller;
  */
 class CrutchController extends Controller
 {
+    /**
+     * Saves all characters
+     * @return void
+     */
+    public function actionSaveCharacters()
+    {
+        $objects = Character::find()->all();
+
+        foreach ($objects as $object) {
+            $object->save();
+        }
+    }
+    /**
+     * Saves all characters
+     * @return void
+     */
+    public function actionSaveCharacterSheets()
+    {
+        $objects = CharacterSheet::find()->all();
+
+        foreach ($objects as $object) {
+            $object->save();
+        }
+    }
+
+    /**
+     * Saves all groups
+     * @return void
+     */
+    public function actionSaveGroups()
+    {
+        $objects = Group::find()->all();
+
+        foreach ($objects as $object) {
+            $object->save();
+        }
+    }
+
+    /**
+     * Saves all groups
+     * @return void
+     */
+    public function actionSaveEpics()
+    {
+        $objects = Epic::find()->all();
+
+        foreach ($objects as $object) {
+            $object->save();
+        }
+    }
+
+    /**
+     * Saves all groups
+     * @return void
+     */
+    public function actionSaveRecaps()
+    {
+        $objects = Recap::find()->all();
+
+        foreach ($objects as $object) {
+            $object->save();
+        }
+    }
+
+    /**
+     * Saves all groups
+     * @return void
+     */
+    public function actionSaveStories()
+    {
+        $objects = Story::find()->all();
+
+        foreach ($objects as $object) {
+            $object->save();
+        }
+    }
+
+    public function actionSaveAll()
+    {
+        $this->runAction('save-epics');
+        $this->runAction('save-stories');
+        $this->runAction('save-recaps');
+        $this->runAction('save-characters');
+        $this->runAction('save-character-sheets');
+        $this->runAction('save-groups');
+    }
 }
