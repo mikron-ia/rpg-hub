46cec3d6a1152e5b121ffef0b3dfe02fb82fddea
Mikron <mikronia@gmail.com>
2016-12-26 13:50:10 +0100

    Information about registering moved to frontpage as well

--- frontend/views/site/user/accept.php
+++ frontend/views/site/user/accept.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model common\models\User */
+/* @var $model common\models\user\UserAcceptForm */
 
 $this->title = Yii::t('app', 'USER_ACCOUNT_CREATE');
 $this->params['breadcrumbs'][] = $this->title;
@@ -11,6 +11,16 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="user-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
+
+    <p><?= Yii::t('app', 'USER_ACCEPT_WARNING') ?></p>
+    <p><?= Yii::t('app', 'USER_ACCEPT_TLDR') ?></p>
+    <p>
+        <?= Yii::t(
+            'app',
+            'USER_ACCEPT_EXPIRATION_WARNING {when}',
+            ['when' => date('Y-m-d H:i:s', $model->getExpirationTime())]
+        ) ?>
+    </p>
 
     <?= $this->render('_form_accept', [
         'model' => $model,
