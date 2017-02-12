6d08f15fc7aeb993388d55fe54f7df4a91db85b5
Mikron <mikronia@gmail.com>
2017-02-12 18:15:33 +0100

    Description display in Character backend made more tablet-friendly

--- backend/views/character/_view_description.php
+++ backend/views/character/_view_description.php
@@ -7,85 +7,87 @@ use yii\helpers\Html;
 
 ?>
 
-<div class="col-md-6">
+<div class="col-md-12">
 
     <div class="buttoned-header">
         <h3><?= $model->position; ?>. <?= $model->getTypeName(); ?></h3>
-
-        <?= Html::a(
-            Yii::t('app', 'BUTTON_SHOW_HISTORY'),
-            '#',
-            [
-                'class' => 'btn btn-default description-history-link',
-                'data-toggle' => 'modal',
-                'data-target' => '#description-history-modal',
-                'data-id' => $model->description_id,
-            ]
-        ); ?>
-        <?= Html::a(
-            Yii::t('app', 'BUTTON_UPDATE'),
-            '#',
-            [
-                'class' => 'btn btn-primary update-description-link',
-                'title' => Yii::t('app', 'LABEL_UPDATE'),
-                'data-toggle' => 'modal',
-                'data-target' => '#update-description-modal',
-                'data-id' => $model->description_id,
-            ]
-        ); ?>
-        <?= Html::a(
-            Yii::t('app', 'BUTTON_MOVE_DOWN'),
-            ['description/move-down', 'id' => $model->description_id],
-            [
-                'class' => 'btn btn-default',
-                'data' => [
-                    'method' => 'post',
-                ],
-            ]
-        ); ?>
-        <?= Html::a(
-            Yii::t('app', 'BUTTON_MOVE_UP'),
-            ['description/move-up', 'id' => $model->description_id],
-            [
-                'class' => 'btn btn-default',
-                'data' => [
-                    'method' => 'post',
-                ],
-            ]
-        ); ?>
-        <?= Html::a(
-            Yii::t('app', 'BUTTON_DELETE'),
-            ['description/delete', 'id' => $model->description_id],
-            [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => Yii::t('app', 'CONFIRMATION_DELETE'),
-                    'method' => 'post',
-                ],
-            ]
-        ); ?>
+        <div>
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_MOVE_DOWN'),
+                ['description/move-down', 'id' => $model->description_id],
+                [
+                    'class' => 'btn btn-default',
+                    'data' => [
+                        'method' => 'post',
+                    ],
+                ]
+            ); ?>
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_MOVE_UP'),
+                ['description/move-up', 'id' => $model->description_id],
+                [
+                    'class' => 'btn btn-default',
+                    'data' => [
+                        'method' => 'post',
+                    ],
+                ]
+            ); ?>
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_UPDATE'),
+                '#',
+                [
+                    'class' => 'btn btn-primary update-description-link',
+                    'title' => Yii::t('app', 'LABEL_UPDATE'),
+                    'data-toggle' => 'modal',
+                    'data-target' => '#update-description-modal',
+                    'data-id' => $model->description_id,
+                ]
+            ); ?>
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_DELETE'),
+                ['description/delete', 'id' => $model->description_id],
+                [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('app', 'CONFIRMATION_DELETE'),
+                        'method' => 'post',
+                    ],
+                ]
+            ); ?>
+            
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_SHOW_HISTORY'),
+                '#',
+                [
+                    'class' => 'btn btn-default description-history-link',
+                    'data-toggle' => 'modal',
+                    'data-target' => '#description-history-modal',
+                    'data-id' => $model->description_id,
+                ]
+            ); ?>
+        </div>
     </div>
 
-    <p>
-        <span class="tag-box">
-            <?= Yii::t('app', 'LABEL_VISIBLE') . ' ' . $model->getVisibilityLowercase(); ?>
-        </span>
-        <span class="tag-box">
-            <?= $model->getLanguage(); ?>
-        </span>
+    <div>
         <span class="tag-box">
             <?= Yii::t('app', 'DESCRIPTION_UPDATED {when} {who}', [
                 'when' => Yii::$app->formatter->asDatetime($model->updated_at),
                 'who' => $model->updatedBy->username,
             ]); ?>
         </span>
-    </p>
+        <span class="tag-box">
+            <?= Yii::t('app', 'LABEL_VISIBLE') . ' ' . $model->getVisibilityLowercase(); ?>
+        </span>
+        <span class="tag-box">
+            <?= $model->getLanguage(); ?>
+        </span>
+    </div>
 
-    <div>
+    <div class="col-md-6">
         <?= $model->getPublicFormatted(); ?>
     </div>
 
-    <div class="private-notes">
+    <div class="col-md-6 private-notes">
         <?= $model->getPrivateFormatted(); ?>
     </div>
 
