3d154e3f770905aba84c18c83a1e6dfd7e83fdef
Mikron <mikronia@gmail.com>
2017-04-30 23:59:02 +0200

    Group memberships are now displayed in simplified way
    Concerns #34

--- frontend/views/group/_view_descriptions.php
+++ frontend/views/group/_view_descriptions.php
@@ -4,32 +4,53 @@
 /* @var $showPrivates bool */
 ?>
 
-<?php if ($model->descriptionPack): ?>
-    <div id="descriptions">
+<div class="col-md-9">
+    <?php if ($model->descriptionPack): ?>
+        <div id="descriptions">
 
-        <?= \yii\widgets\ListView::widget([
-            'dataProvider' => new \yii\data\ActiveDataProvider([
-                'query' => $model->descriptionPack->getDescriptionsInLanguageOfTheActiveUser(),
-                'sort' => ['defaultOrder' => ['position' => SORT_ASC]]
-            ]),
-            'itemOptions' => ['class' => 'item'],
-            'summary' => '',
-            'itemView' => function (\common\models\Description $model, $key, $index, $widget) {
-                return $this->render(
-                    '_view_description',
-                    [
-                        'model' => $model,
-                        'key' => $key,
-                        'index' => $index,
-                        'widget' => $widget,
-                        'showPrivates' => $this->params['showPrivates']
-                    ]
-                );
-            },
-        ]) ?>
+            <?= \yii\widgets\ListView::widget([
+                'dataProvider' => new \yii\data\ActiveDataProvider([
+                    'query' => $model->descriptionPack->getDescriptionsInLanguageOfTheActiveUser(),
+                    'sort' => ['defaultOrder' => ['position' => SORT_ASC]]
+                ]),
+                'itemOptions' => ['class' => 'item'],
+                'summary' => '',
+                'itemView' => function (\common\models\Description $model, $key, $index, $widget) {
+                    return $this->render(
+                        '_view_description',
+                        [
+                            'model' => $model,
+                            'key' => $key,
+                            'index' => $index,
+                            'widget' => $widget,
+                            'showPrivates' => $this->params['showPrivates']
+                        ]
+                    );
+                },
+            ]) ?>
 
-    </div>
-<?php else: ?>
-    <p><?= Yii::t('app', 'DESCRIPTIONS_NOT_FOUND'); ?></p>
-<?php endif; ?>
+        </div>
+    <?php else: ?>
+        <p><?= Yii::t('app', 'DESCRIPTIONS_NOT_FOUND'); ?></p>
+    <?php endif; ?>
+</div>
 
+<div class="col-md-3">
+    <h3 class="center">Members</h3>
+    <?php if ($model->groupCharacterMemberships): ?>
+        <div id="memberships">
+
+            <?= \yii\grid\GridView::widget([
+                'dataProvider' => new \yii\data\ArrayDataProvider(['allModels' => $model->groupCharacterMemberships]),
+                'summary' => '',
+                'columns' => [
+                    'character.name',
+                    'short_text',
+                ],
+            ]); ?>
+
+        </div>
+    <?php else: ?>
+        <p><?= Yii::t('app', 'MEMBERSHIPS_NOT_FOUND'); ?></p>
+    <?php endif; ?>
+</div>
