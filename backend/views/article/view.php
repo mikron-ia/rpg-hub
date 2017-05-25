10b0684cbf6727bfca038381723288dd9194b59d
Mikron <mikronia@gmail.com>
2017-05-25 09:27:23 +0200

    Article view redesigned

--- backend/views/article/view.php
+++ backend/views/article/view.php
@@ -12,40 +12,65 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="article-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="buttoned-header">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <?= Html::a(
+            Yii::t('app', 'BUTTON_UPDATE'),
+            ['update', 'id' => $model->article_id],
+            ['class' => 'btn btn-primary']
+        ) ?>
+        <?= Html::a(
+            Yii::t('app', 'BUTTON_DELETE'),
+            ['delete', 'id' => $model->article_id],
+            [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => Yii::t('app', 'CONFIRMATION_DELETE'),
+                    'method' => 'post',
+                ],
+            ]
+        ) ?>
+    </div>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'BUTTON_UPDATE'), ['update', 'id' => $model->article_id],
-            ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'BUTTON_DELETE'), ['delete', 'id' => $model->article_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'CONFIRMATION_DELETE'),
-                'method' => 'post',
+    <p class="subtitle"><?= $model->subtitle ?></p>
+
+    <div class="col-md-6">
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                [
+                    'attribute' => 'epic_id',
+                    'format' => 'raw',
+                    'value' => $model->epic_id
+                        ? (Html::a($model->epic->name, ['epic/view', 'id' => $model->epic_id], []))
+                        : Yii::t('app', 'ARTICLE_NO_EPIC'),
+                ],
+                'key',
+                [
+                    'attribute' => 'visibility',
+                    'value' => $model->getVisibility()
+                ],
             ],
         ]) ?>
-    </p>
+    </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'attribute' => 'epic_id',
-                'format' => 'raw',
-                'value' => $model->epic_id
-                    ? (Html::a($model->epic->name, ['epic/view', 'id' => $model->epic_id], []))
-                    : Yii::t('app', 'ARTICLE_NO_EPIC'),
+    <div class="col-md-6">
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                [
+                    'label' => 'ARTICLE_CHARACTER_COUNT',
+                    'value' => '?'
+                ],
+                [
+                    'label' => 'ARTICLE_WORD_COUNT',
+                    'value' => '?'
+                ],
             ],
-            'key',
-            'subtitle',
-            [
-                'attribute' => 'visibility',
-                'value' => $model->getVisibility()
-            ],
-        ],
-    ]) ?>
+        ]) ?>
+    </div>
 
-    <div>
+    <div class="col-md-12">
         <?= $model->text_ready ?>
     </div>
 
