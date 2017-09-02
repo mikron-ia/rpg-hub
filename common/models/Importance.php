d4d17ffa589d5019a93744c5d85278db71f67677
Mikron <mikronia@gmail.com>
2017-09-03 00:22:05 +0200

    Importance calculation methods

--- common/models/Importance.php
+++ common/models/Importance.php
@@ -2,6 +2,7 @@
 
 namespace common\models;
 
+use common\models\core\ImportanceCategory;
 use Yii;
 
 /**
@@ -26,8 +27,20 @@ class Importance extends \yii\db\ActiveRecord
     {
         return [
             [['importance_pack_id', 'user_id', 'importance'], 'integer'],
-            [['importance_pack_id'], 'exist', 'skipOnError' => true, 'targetClass' => ImportancePack::className(), 'targetAttribute' => ['importance_pack_id' => 'importance_pack_id']],
-            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [
+                ['importance_pack_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => ImportancePack::className(),
+                'targetAttribute' => ['importance_pack_id' => 'importance_pack_id']
+            ],
+            [
+                ['user_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => User::className(),
+                'targetAttribute' => ['user_id' => 'id']
+            ],
         ];
     }
 
@@ -64,5 +77,71 @@ class Importance extends \yii\db\ActiveRecord
     public function recalculate():bool
     {
         return true;
+    }
+
+    private function determineValueBasedOnSeen(string $seen):int
+    {
+        switch ($seen) {
+            case 'new' :
+                $result = 128;
+                break;
+            case 'modified' :
+                $result = 64;
+                break;
+            default:
+                $result = 0;
+                break;
+        }
+
+        return $result;
+    }
+
+    private function determineValueBasedOnImportanceCategory(string $importanceCategory)
+    {
+        switch ($importanceCategory) {
+            case ImportanceCategory::IMPORTANCE_EXTREME :
+                $result = 64;
+                break;
+            case ImportanceCategory::IMPORTANCE_HIGH:
+                $result = 32;
+                break;
+            case ImportanceCategory::IMPORTANCE_MEDIUM:
+                $result = 16;
+                break;
+            case ImportanceCategory::IMPORTANCE_LOW:
+                $result = 8;
+                break;
+            case ImportanceCategory::IMPORTANCE_NONE:
+                $result = 0;
+                break;
+            default:
+                $result = 0;
+                break;
+        }
+
+        return $result;
+    }
+
+    private function determineValueBasedOnAssociation(bool $isAssociated):int
+    {
+        return $isAssociated ? 8 : 0;
+    }
+
+    private function determineValueBasedOnDate(\DateTimeImmutable $date, $topValue):int
+    {
+        $now = new \DateTimeImmutable('now');
+        $difference = $date->diff($now);
+
+        if ($difference->y > 0) {
+            $result = $topValue / 8;
+        } elseif ($difference->m > 0) {
+            $result = $topValue / 4;
+        } elseif ($difference->w > 0) {
+            $result = $topValue / 2;
+        } else {
+            $result = $topValue;
+        }
+
+        return (int)round($result);
     }
 }
