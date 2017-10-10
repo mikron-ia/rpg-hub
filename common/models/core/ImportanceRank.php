cdb3dfaf680ba3701fad260d768869b77d186a38
Mikron <mikronia@gmail.com>
2017-10-10 23:56:02 +0200

    ImportanceRank object expanded
    Concerns #252

--- common/models/core/ImportanceRank.php
+++ common/models/core/ImportanceRank.php
@@ -10,11 +10,27 @@ use Yii;
  */
 final class ImportanceRank
 {
-    const IMPORTANCE_RANK_LOWEST = 'lowest';
+    const IMPORTANCE_RANK_EXTREME_LOW = 'extreme-low';
     const IMPORTANCE_RANK_LOW = 'low';
+    const IMPORTANCE_RANK_MEDIUM_LOW = 'medium-low';
     const IMPORTANCE_RANK_MEDIUM = 'medium';
+    const IMPORTANCE_RANK_MEDIUM_HIGH = 'medium-high';
     const IMPORTANCE_RANK_HIGH = 'high';
-    const IMPORTANCE_RANK_HIGHEST = 'extreme';
+    const IMPORTANCE_RANK_EXTREMELY_HIGH = 'extreme-high';
+
+    const IMPORTANCE_RANK_INCORRECT = 'incorrect';
+    const IMPORTANCE_RANK_UNKNOWN = 'unknown';
+
+    const IMPORTANCE_RANK_CODES = [
+        self::IMPORTANCE_RANK_EXTREME_LOW,
+        self::IMPORTANCE_RANK_LOW,
+        self::IMPORTANCE_RANK_MEDIUM_LOW,
+        self::IMPORTANCE_RANK_MEDIUM,
+        self::IMPORTANCE_RANK_MEDIUM_HIGH,
+        self::IMPORTANCE_RANK_HIGH,
+        self::IMPORTANCE_RANK_EXTREMELY_HIGH,
+        self::IMPORTANCE_RANK_UNKNOWN,
+    ];
 
     /**
      * @var string
@@ -28,11 +44,15 @@ final class ImportanceRank
     static public function importanceNames(): array
     {
         $names = [
-            self::IMPORTANCE_RANK_LOWEST => Yii::t('app', 'IMPORTANCE_RANK_LOWEST'),
-            self::IMPORTANCE_RANK_LOW => Yii::t('app', 'IMPORTANCE_RANK_LOW_LOWERCASE'),
-            self::IMPORTANCE_RANK_MEDIUM => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM_LOWERCASE'),
-            self::IMPORTANCE_RANK_HIGH => Yii::t('app', 'IMPORTANCE_RANK_HIGH_LOWERCASE'),
-            self::IMPORTANCE_RANK_HIGHEST => Yii::t('app', 'IMPORTANCE_RANK_HIGHEST_LOWERCASE'),
+            self::IMPORTANCE_RANK_EXTREME_LOW => Yii::t('app', 'IMPORTANCE_RANK_EXTREME_LOW'),
+            self::IMPORTANCE_RANK_LOW => Yii::t('app', 'IMPORTANCE_RANK_LOW'),
+            self::IMPORTANCE_RANK_MEDIUM_LOW => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM_LOW'),
+            self::IMPORTANCE_RANK_MEDIUM => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM'),
+            self::IMPORTANCE_RANK_MEDIUM_HIGH => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM_HIGH'),
+            self::IMPORTANCE_RANK_HIGH => Yii::t('app', 'IMPORTANCE_RANK_HIGH'),
+            self::IMPORTANCE_RANK_EXTREMELY_HIGH => Yii::t('app', 'IMPORTANCE_RANK_EXTREMELY_HIGH'),
+            self::IMPORTANCE_RANK_UNKNOWN => Yii::t('app', 'IMPORTANCE_RANK_UNKNOWN'),
+            self::IMPORTANCE_RANK_INCORRECT => Yii::t('app', 'IMPORTANCE_RANK_INCORRECT'),
         ];
 
         return $names;
@@ -45,21 +65,31 @@ final class ImportanceRank
     static public function importanceNamesLowercase(): array
     {
         return [
-            self::IMPORTANCE_RANK_LOWEST => Yii::t('app', 'IMPORTANCE_RANK_LOWEST_LOWERCASE'),
+            self::IMPORTANCE_RANK_EXTREME_LOW => Yii::t('app', 'IMPORTANCE_RANK_EXTREME_LOW_LOWERCASE'),
             self::IMPORTANCE_RANK_LOW => Yii::t('app', 'IMPORTANCE_RANK_LOW_LOWERCASE'),
+            self::IMPORTANCE_RANK_MEDIUM_LOW => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM_LOW_LOWERCASE'),
             self::IMPORTANCE_RANK_MEDIUM => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM_LOWERCASE'),
+            self::IMPORTANCE_RANK_MEDIUM_HIGH => Yii::t('app', 'IMPORTANCE_RANK_MEDIUM_HIGH_LOWERCASE'),
             self::IMPORTANCE_RANK_HIGH => Yii::t('app', 'IMPORTANCE_RANK_HIGH_LOWERCASE'),
-            self::IMPORTANCE_RANK_HIGHEST => Yii::t('app', 'IMPORTANCE_RANK_HIGHEST_LOWERCASE'),
+            self::IMPORTANCE_RANK_EXTREMELY_HIGH => Yii::t('app', 'IMPORTANCE_RANK_EXTREMELY_HIGH_LOWERCASE'),
+            self::IMPORTANCE_RANK_UNKNOWN => Yii::t('app', 'IMPORTANCE_RANK_UNKNOWN_LOWERCASE'),
+            self::IMPORTANCE_RANK_INCORRECT => Yii::t('app', 'IMPORTANCE_RANK_INCORRECT_LOWERCASE'),
         ];
     }
 
     /**
+     * Creates ImportanceRank object from string code
      * @param $code
      * @return ImportanceRank
      */
     static public function create($code): ImportanceRank
     {
         $importance = new ImportanceRank();
+
+        if (!in_array($code, self::IMPORTANCE_RANK_CODES)) {
+            $code = self::IMPORTANCE_RANK_INCORRECT;
+        }
+
         $importance->importance = $code;
         return $importance;
     }
