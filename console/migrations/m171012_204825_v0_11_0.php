601514ff909efa891bcac95f956325122d234ac6
Mikron <mikronia@gmail.com>
2017-12-12 01:29:08 +0100

    Points in time migration
    Concerns #266

--- console/migrations/m171012_204825_v0_11_0.php
+++ console/migrations/m171012_204825_v0_11_0.php
@@ -6,6 +6,9 @@ class m171012_204825_v0_11_0 extends Migration
 {
     public function safeUp()
     {
+        $tableOptions = "CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB";
+
+        /* Description rework */
         $this->addColumn('description', 'public_text_expanded', $this->text()->after('private_text'));
         $this->addColumn('description', 'protected_text_expanded', $this->text()->after('public_text_expanded'));
         $this->addColumn('description', 'private_text_expanded', $this->text()->after('protected_text_expanded'));
@@ -21,10 +24,21 @@ class m171012_204825_v0_11_0 extends Migration
         $this->execute('UPDATE description_history SET public_text_expanded = public_text');
         $this->execute('UPDATE description_history SET protected_text_expanded = protected_text');
         $this->execute('UPDATE description_history SET private_text_expanded = private_text');
+
+        /* Points in time */
+        $this->createTable('point_in_time', [
+            'point_in_time_id' => $this->primaryKey()->unsigned(),
+            'epic_id' => $this->integer()->unsigned()->notNull(),
+            'name' => $this->string(255)->notNull(),
+            'description' => $this->string(255),
+            'position' => $this->integer()->unsigned()->notNull()
+        ], $tableOptions);
     }
 
     public function safeDown()
     {
+        $this->dropTable('point_in_time');
+
         $this->dropColumn('description_history', 'public_text_expanded');
         $this->dropColumn('description_history', 'protected_text_expanded');
         $this->dropColumn('description_history', 'private_text_expanded');
