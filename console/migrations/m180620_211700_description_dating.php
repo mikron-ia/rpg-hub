bf27c90786472a2fb318a0c5d0b3a491f6cbf227
Mikron <mikronia@gmail.com>
2018-06-20 23:21:28 +0200

    Description dating migration
    Concerns #285

---
+++ console/migrations/m180620_211700_description_dating.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180620_211700_description_dating
+ */
+class m180620_211700_description_dating extends Migration
+{
+
+    public function safeUp()
+    {
+        $this->addColumn('description', 'point_in_time_id', $this->integer()->unsigned());
+
+        $this->addForeignKey(
+            'description_point_in_time',
+            'description', 'point_in_time_id',
+            'point_in_time', 'point_in_time_id',
+            'RESTRICT', 'CASCADE'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('description_point_in_time', 'description');
+
+        $this->dropColumn('description', 'point_in_time_id');
+    }
+}
