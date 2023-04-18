f71c317f147d8c35e05a5d21d573f427ccedfdb6
Mikron <mikronia@gmail.com>
2023-04-19 01:11:44 +0200

    Fixed migration - foreign keys are now correctly named
    Concerns #356

--- console/migrations/m230417_220343_expiration_description_dating.php
+++ console/migrations/m230417_220343_expiration_description_dating.php
@@ -13,7 +13,7 @@ class m230417_220343_expiration_description_dating extends Migration
         $this->addColumn('description_history', 'point_in_time_end_id', $this->integer()->unsigned());
 
         $this->addForeignKey(
-            'description_expiration_point_in_time',
+            'description_point_in_time_end',
             'description', 'point_in_time_end_id',
             'point_in_time', 'point_in_time_id',
             'RESTRICT', 'CASCADE'
@@ -28,15 +28,49 @@ class m230417_220343_expiration_description_dating extends Migration
 
         $this->renameColumn('description', 'point_in_time_id', 'point_in_time_start_id');
         $this->renameColumn('description_history', 'point_in_time_id', 'point_in_time_start_id');
+
+        $this->dropForeignKey('description_point_in_time', 'description');
+        $this->dropForeignKey('description_history_point_in_time', 'description_history');
+
+        $this->addForeignKey(
+            'description_point_in_time_start',
+            'description', 'point_in_time_start_id',
+            'point_in_time', 'point_in_time_id',
+            'RESTRICT', 'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'description_history_point_in_time_start',
+            'description_history', 'point_in_time_start_id',
+            'point_in_time', 'point_in_time_id',
+            'RESTRICT', 'CASCADE'
+        );
     }
 
     public function safeDown()
     {
+        $this->dropForeignKey('description_history_point_in_time_start', 'description_history');
+        $this->dropForeignKey('description_point_in_time_start', 'description');
+
+        $this->addForeignKey(
+            'description_history_point_in_time_start',
+            'description_history', 'point_in_time_start_id',
+            'point_in_time', 'point_in_time_id',
+            'RESTRICT', 'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'description_point_in_time_start',
+            'description', 'point_in_time_start_id',
+            'point_in_time', 'point_in_time_id',
+            'RESTRICT', 'CASCADE'
+        );
+
         $this->renameColumn('description_history', 'point_in_time_start_id', 'point_in_time_id');
         $this->renameColumn('description', 'point_in_time_start_id', 'point_in_time_id');
 
         $this->dropForeignKey('description_history_expiration_point_in_time', 'description_history');
-        $this->dropForeignKey('description_expiration_point_in_time', 'description');
+        $this->dropForeignKey('description_point_in_time_end', 'description');
 
         $this->dropColumn('description_history', 'point_in_time_end_id');
         $this->dropColumn('description', 'point_in_time_end_id');
