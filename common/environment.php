eec832b19dfa6eea279d0282625e5e9bbd9c720b
Mikron <mikronia@gmail.com>
2023-05-07 12:47:20 +0200

    Dependencies update
    * dotenv version raised
    * necessary adjustments to codebase made

--- common/environment.php
+++ common/environment.php
@@ -1,6 +1,6 @@
 <?php
 
-$environment = new \Dotenv\Dotenv(dirname(__DIR__));
+$environment = \Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__)); // either use unsafe or remove all getenv()
 $environment->load();
 
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
