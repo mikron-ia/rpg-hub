f24e1ae56ad945a2d78468cf22fe95f9d9cf4e0a
Mikron <mikronia@gmail.com>
2023-10-06 23:59:45 +0200

    Simple adding to favorites
    Concerns #253

--- frontend/views/scribble/_modal_box.php
+++ frontend/views/scribble/_modal_box.php
@@ -1,21 +1,43 @@
 <?php
 
-use yii\widgets\DetailView;
+use yii\helpers\Html;
 
 /** @var yii\web\View $this */
 /** @var common\models\Scribble $model */
 
+$favoriteButtonTexts = [
+    false => Yii::t('app', 'SCRIBBLES_TITLE_NO'),
+    true => Yii::t('app', 'SCRIBBLES_TITLE_YES'),
+];
+
 ?>
 <div class="scribble-view">
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'scribble_id',
-            'scribble_pack_id',
-            'user_id',
-            'favorite',
-        ],
+    <?= Html::button($favoriteButtonTexts[$model->favorite], [
+        'id' => 'favorite-button',
+        'class' => 'btn btn-primary btn-block',
+        'data-scribble-id' => $model->scribble_id,
     ]) ?>
 
 </div>
+
+<script>
+    let favorite = <?= (int)$model->favorite ?>;
+    const isFavoriteText = "<?= $favoriteButtonTexts[true] ?>";
+    const isNotFavoriteText = "<?= $favoriteButtonTexts[false]  ?>";
+
+    $('#favorite-button').on('click', function () {
+        $.ajax(
+            '../scribble/reverse-favorite',
+            {
+                method: "GET",
+                data: {
+                    id: <?= $model->scribble_id ?>
+                }
+            }
+        ).done(function () {
+            favorite = !favorite;
+            $(this).val(favorite ? isFavoriteText : isNotFavoriteText);
+        });
+    });
+</script>
