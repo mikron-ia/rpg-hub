55d7fe75f6f9876cb88ff87dce7a472a7f3b3875
Mikron <mikronia@gmail.com>
2024-07-12 21:50:45 +0200

    Article now has button linking to CMS in the front
    Closes #387

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -10,11 +10,20 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => $model->epic->name, 'url' => ['epic/view', 'key' => $model->epic->key]];
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'ARTICLE_TITLE_INDEX'), 'url' => ['index', 'key' => $model->epic->key]];
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->params['showPrivates'] = $model->canUserControlYou();
 ?>
 <div class="article-view">
 
     <div class="buttoned-header">
         <h1><?= Html::encode($this->title) ?></h1>
+        <?php if ($this->params['showPrivates']): ?>
+            <?= Html::a(
+                Yii::t('app', 'BUTTON_SEE_BACKEND'),
+                Yii::$app->params['uri.back'] . Yii::$app->urlManager->createUrl(['article/view', 'key' => $model->key]),
+                ['class' => 'btn btn-default']
+            ) ?>
+        <?php endif; ?>
     </div>
 
     <p class="subtitle"><?= $model->subtitle ?></p>
